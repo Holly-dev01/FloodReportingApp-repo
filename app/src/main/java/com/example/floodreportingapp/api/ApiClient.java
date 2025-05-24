ec30b1918d9ce7cce530d88c0630f835cede5b69
Holly-dev01 <shimaholly1@gmail.com>
2025-05-24 19:11:36 +0200

    add getApiService method to ApiClient

--- app/src/main/java/com/example/floodreportingapp/api/ApiClient.java
+++ app/src/main/java/com/example/floodreportingapp/api/ApiClient.java
@@ -11,6 +11,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
     private static final String BASE_URL = "http://192.168.43.204:8080/"; //adresse IP de l'ordi
     private static Retrofit retrofit;
+    private static ApiService apiService;
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
@@ -37,5 +38,12 @@ public class ApiClient {
                     .build();
         }
         return retrofit;
+    }
+
+    public static ApiService getApiService() {
+        if (apiService == null) {
+            apiService = getRetrofitInstance().create(ApiService.class);
+        }
+        return apiService;
     }
 }
