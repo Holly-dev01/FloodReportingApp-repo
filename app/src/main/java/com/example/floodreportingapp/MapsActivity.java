710e37d1fd621c9d018c4456479814c99210d09f
Holly-dev01 <shimaholly1@gmail.com>
2025-05-24 21:54:09 +0200

    Add flood report markers to map

--- app/src/main/java/com/example/floodreportingapp/MapsActivity.java
+++ app/src/main/java/com/example/floodreportingapp/MapsActivity.java
@@ -4,11 +4,18 @@ import android.os.Bundle;
 
 import androidx.fragment.app.FragmentActivity;
 
+import com.example.floodreportingapp.model.FloodReportDTO;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
@@ -28,5 +35,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         mMap.getUiSettings().setZoomControlsEnabled(true);
+    }
+
+    // MapsActivity.java
+    private void loadFloodReports() {
+        Call<List<FloodReportDTO>> call = apiService.getAllReports();
+        call.enqueue(new Callback<List<FloodReportDTO>>() {
+            @Override
+            public void onResponse(Call<List<FloodReportDTO>> call, Response<List<FloodReportDTO>> response) {
+                for (FloodReportDTO report : response.body()) {
+                    LatLng position = new LatLng(report.getLatitude(), report.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(position).title(report.getType()));
+                }
+            }
+            // ...
+        });
     }
 }
