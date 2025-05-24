ea8a20a3acac4a6e036760b82b3e005c5c061b36
Holly-dev01 <shimaholly1@gmail.com>
2025-05-24 19:00:10 +0200

    add AuthResponseDTO model

---
+++ app/src/main/java/com/example/floodreportingapp/model/AuthResponseDTO.java
@@ -0,0 +1,18 @@
+package com.example.floodreportingapp.model;
+
+public class AuthResponseDTO {
+    private boolean success;
+    private String token;
+    private String message;
+
+    public AuthResponseDTO() {}
+
+    public boolean isSuccess() { return success; }
+    public void setSuccess(boolean success) { this.success = success; }
+
+    public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+}
