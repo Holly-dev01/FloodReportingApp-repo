1d9009f3c00ecf588e80d080c990acafd0611551
Holly-dev01 <shimaholly1@gmail.com>
2025-05-24 22:37:59 +0200

    Create ReportListActivity

---
+++ app/src/main/java/com/example/floodreportingapp/ReportListActivity.java
@@ -0,0 +1,25 @@
+package com.example.floodreportingapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ReportListActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ReportAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_list);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
