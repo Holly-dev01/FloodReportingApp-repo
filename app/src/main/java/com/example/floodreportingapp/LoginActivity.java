039ba9f3f7e204dda5ed39022f2ca4e0ec97654b
Holly-dev01 <shimaholly1@gmail.com>
2025-05-24 23:35:20 +0200

    Update LoginActivity.java
    for user authentification

--- app/src/main/java/com/example/floodreportingapp/LoginActivity.java
+++ app/src/main/java/com/example/floodreportingapp/LoginActivity.java
@@ -1,24 +1,43 @@
 package com.example.floodreportingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.example.floodreportingapp.utils.SharedPreferencesHelper;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText etUsername, etPassword;
+    private Button btnLogin;
+    private SharedPreferencesHelper prefsHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        initializeViews();
+        initializeServices();
+
+        if (prefsHelper.isLoggedIn()) {
+            navigateToMain();
+        }
+    }
+
+    private void initializeViews() {
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+    }
+
+    private void initializeServices() {
+        prefsHelper = new SharedPreferencesHelper(this);
+    }
+
+    private void navigateToMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
