8cb7f769b43f7b6669caadf1ce7e944576517f2a
Holly-dev01 <shimaholly1@gmail.com>
2025-05-25 22:22:49 +0200

    adding desoign on activity_maim

--- app/src/main/java/com/example/floodreportingapp/utils/SharedPreferencesHelper.java
+++ app/src/main/java/com/example/floodreportingapp/utils/SharedPreferencesHelper.java
@@ -8,12 +8,16 @@ public class SharedPreferencesHelper {
     private static final String PREF_NAME = "FloodReportingAppPrefs";
     private static final String KEY_IS_LOGGED_IN = "is_logged_in";
     private static final String KEY_AUTH_TOKEN = "auth_token";
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_PASSWORD = "password";
     private static final String KEY_DEVICE_ID = "device_id";
 
     private SharedPreferences preferences;
     private SharedPreferences.Editor editor;
+    private Context context;
 
     public SharedPreferencesHelper(Context context) {
+        this.context = context;
         this.preferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
         this.editor = preferences.edit();
     }
@@ -39,7 +43,19 @@ public class SharedPreferencesHelper {
     public void saveCredentials(String username, String password) {
         editor.putString("username", username);
         editor.putString("password", password);
-        editor.apply();
+        editor.commit();
+    }
+
+    public String getSavedUsername() {
+        return preferences.getString(KEY_USERNAME, null);
+    }
+
+    public String getSavedPassword() {
+        return preferences.getString(KEY_PASSWORD, null);
+    }
+
+    public boolean hasSavedCredentials() {
+        return getSavedUsername() != null && getSavedPassword() != null;
     }
 
     public String getDeviceId() {
