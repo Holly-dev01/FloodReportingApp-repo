1b3d243c52e90a29d9c0c52fb5b8e38efc1994e3
Holly-dev01 <shimaholly1@gmail.com>
2025-05-25 21:37:08 +0200

    adding dependancies for biometric and

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id("com.android.application")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -34,15 +35,30 @@ android {
 dependencies {
     // Google Maps
     implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+
+    // Biometric Authentication
+    implementation("androidx.biometric:biometric:1.1.0")
+
     // Network
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
+    // RecyclerView
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+    // SharedPreferences
+    implementation("androidx.preference:preference:1.2.1")
+
+    // CardView
+    implementation("androidx.cardview:cardview:1.0.0")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation(libs.activity)
+    //Google maps
     implementation(libs.play.services.maps)
+    // SwipeRefreshLayout
     implementation(libs.swiperefreshlayout)
 }
